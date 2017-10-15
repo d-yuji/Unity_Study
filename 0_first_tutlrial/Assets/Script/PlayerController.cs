69cfee401effd45a43efae4b6f2513e94551f9e4
d.yuji <bronzsord@gmail.com>
2017-10-15 18:43:09 +0900

    first tutorilal part2

---
+++ 0_first_tutlrial/Assets/Script/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float playerspeed = 10;
+    private void FixedUpdate()
+    {
+        float pX = Input.GetAxis("Horizontal");
+        float pZ = Input.GetAxis("Vertical");
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(pX*playerspeed, 0, pZ*playerspeed);
+    }
+}
