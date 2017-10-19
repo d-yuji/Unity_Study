90fedaad3920c6f29e60e7b3b9e7b6ed70a18d8a
d.yuji <bronzsord@gmail.com>
2017-10-19 18:53:13 +0900

    自機の操作が可能にした

---
+++ 1_2d-shooting-game/2d-shooting-game-master/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float playerX = Input.GetAxisRaw("Horizontal");
+        float playerY = Input.GetAxisRaw("Vertical");
+        
+        // 移動方向をベクトルとして作成
+        Vector2 direction = new Vector2(playerX, playerY).normalized;
+        
+        // 移動する向きとスピードを代入する
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+	}
+}
