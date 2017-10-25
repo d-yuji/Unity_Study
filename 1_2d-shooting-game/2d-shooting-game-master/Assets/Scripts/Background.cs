60fb69dbc8d636f337d07e1ded99c1a18b9e1aca
d.yuji <bronzsord@gmail.com>
2017-10-25 20:36:30 +0900

    無限waveの実装

---
+++ 1_2d-shooting-game/2d-shooting-game-master/Assets/Scripts/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float backGroundY = Mathf.Repeat(Time.time * speed, 1);
+        Vector2 offset = new Vector2(0, backGroundY);
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
