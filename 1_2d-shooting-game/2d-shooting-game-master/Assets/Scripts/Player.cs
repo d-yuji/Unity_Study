eb3a1f08c685b3c789a009d0ab150e591bbf5733
d.yuji <bronzsord@gmail.com>
2017-10-26 11:56:06 +0900

    自機の移動制限

--- 1_2d-shooting-game/2d-shooting-game-master/Assets/Scripts/Player.cs
+++ 1_2d-shooting-game/2d-shooting-game-master/Assets/Scripts/Player.cs
@@ -26,8 +26,21 @@ public class Player : MonoBehaviour {
         Vector2 direction = new Vector2(playerX, playerY).normalized;
 
         // 移動する向きとスピードを代入する
-        spaceship.Move(direction);
+        Move(direction);
 	}
+    void Move(Vector2 direction)
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        Vector2 pos = transform.position;
+
+        pos += direction * spaceship.speed * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+        pos.y = Mathf.Clamp(pos.y, min.y, max.y);
+
+        transform.position = pos;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // レイヤー名を取得
