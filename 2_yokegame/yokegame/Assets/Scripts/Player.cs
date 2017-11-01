362c1ea27d64d3739369fb571c37c2dd288ca8c2
d.yuji <bronzsord@gmail.com>
2017-11-01 21:29:05 +0900

    あれこれおかしいのを直して、カメラが追従するようにした

--- 2_yokegame/yokegame/Assets/Scripts/Player.cs
+++ 2_yokegame/yokegame/Assets/Scripts/Player.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     Rigidbody2D playerRigidbody2D;
-    public float jumpValue = 500;
+    public GameObject mainCamera;
+    public float jumpValue = 400;
     public float moveForce = 50;
     public float moveSpeed = 8;
+    bool isGround = false;
+
 	// Use this for initialization
 	void Start () {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
@@ -16,15 +19,28 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float playerX = Input.GetAxisRaw("Horizontal");
-        //float playerY = Input.GetAxisRaw("Vertical");
-        //Vector2 direction = new Vector2(playerX, 0).normalized;
-        //Vector2 pos = transform.position;
-        //pos += direction * speed * Time.deltaTime;
-        //transform.position = pos;
-        playerRigidbody2D.AddForce(moveForce * (Vector2.right * playerX * moveSpeed - playerRigidbody2D.velocity));
-        if (Input.GetKeyDown("space"))
+        if(transform.position.x > mainCamera.transform.position.x - 5)
+        {
+            Vector3 cameraPos = mainCamera.transform.position;
+            cameraPos.x = transform.position.x + 4;
+            mainCamera.transform.position = cameraPos;
+        }
+        if (isGround)
+        {
+            playerRigidbody2D.AddForce(moveForce * (Vector2.right * playerX * moveSpeed - playerRigidbody2D.velocity));
+        }
+        if (Input.GetKeyDown("space") && isGround)
         {
             playerRigidbody2D.AddForce(Vector2.up * jumpValue);
+            isGround = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(collision.gameObject.name);
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
         }
     }
 }
