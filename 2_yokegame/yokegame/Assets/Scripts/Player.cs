8cb9a0c778650dcbbf71a7a6731ff6bb28d71851
d.yuji <bronzsord@gmail.com>
2017-11-01 20:26:15 +0900

    Save

--- 2_yokegame/yokegame/Assets/Scripts/Player.cs
+++ 2_yokegame/yokegame/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour {
 
     Rigidbody2D playerRigidbody2D;
     public float jumpValue = 500;
+    public float moveForce = 50;
+    public float moveSpeed = 8;
 	// Use this for initialization
 	void Start () {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
@@ -15,11 +17,11 @@ public class Player : MonoBehaviour {
 	void Update () {
         float playerX = Input.GetAxisRaw("Horizontal");
         //float playerY = Input.GetAxisRaw("Vertical");
-        Vector2 direction = new Vector2(playerX, 0).normalized;
-        Vector2 pos = transform.position;
-        float speed = 10;
-        pos += direction * speed * Time.deltaTime;
-        transform.position = pos;
+        //Vector2 direction = new Vector2(playerX, 0).normalized;
+        //Vector2 pos = transform.position;
+        //pos += direction * speed * Time.deltaTime;
+        //transform.position = pos;
+        playerRigidbody2D.AddForce(moveForce * (Vector2.right * playerX * moveSpeed - playerRigidbody2D.velocity));
         if (Input.GetKeyDown("space"))
         {
             playerRigidbody2D.AddForce(Vector2.up * jumpValue);
