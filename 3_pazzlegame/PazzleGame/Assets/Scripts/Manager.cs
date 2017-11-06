2d164a6164e6ec14b99c33a8b97344cb96b9abdd
d.yuji <bronzsord@gmail.com>
2017-11-06 17:50:01 +0900

    ブロックのランダム生成

---
+++ 3_pazzlegame/PazzleGame/Assets/Scripts/Manager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Manager : MonoBehaviour {
+    public GameObject player;
+    public GameObject score;
+    public GameObject title;
+
+    public GameObject[] Blocks = new GameObject[5];
+    private float createY = 3.5f;
+    private float[] createX = new float[] {-3.5f,-2.5f, -1.5f, -0.5f, 0.5f, 1.5f, 2.5f, 3.5f, };
+
+
+	// Use this for initialization
+	void Start () {
+        title = GameObject.Find("Title");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(IsPlaying() == false && Input.GetKeyDown(KeyCode.X))
+        {
+            GameStart();
+        }
+	}
+    void GameStart()
+    {
+        title.SetActive(false);
+        int rand = Random.Range(0, 7);
+        int randB = Random.Range(0, 4);
+        Instantiate(player, player.transform.position, player.transform.rotation);
+        Instantiate(Blocks[randB], new Vector3(createX[rand],createY,0), Blocks[randB].transform.rotation);
+    }
+    public void GameOver()
+    {
+
+    }
+
+    private IEnumerator DelayMethond(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+    }
+
+    public bool IsPlaying()
+    {
+        return title.activeSelf == false;
+    }
+}
--- 3_pazzlegame/PazzleGame/Assets/Scripts/Player.cs
+++ 3_pazzlegame/PazzleGame/Assets/Scripts/Player.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    Rigidbody2D playerRigidbody2D;
+
 	// Use this for initialization
 	void Start () {
-		
+        playerRigidbody2D = GetComponent<Rigidbody2D>();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float playerX = Input.GetAxisRaw("Horizontal");
+        float playerY = Input.GetAxisRaw("Vertical");
 	}
 }
